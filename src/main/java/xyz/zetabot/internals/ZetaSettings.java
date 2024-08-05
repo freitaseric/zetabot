f55bb7f4b13cd9b5d6539649449070fc290b91b9
freitaseric <freitaseric@proton.mr>
2024-08-05 11:37:17 -0300

    feat: improve release channel system

--- src/main/java/xyz/zetabot/internals/ZetaSettings.java
+++ src/main/java/xyz/zetabot/internals/ZetaSettings.java
@@ -4,7 +4,7 @@ import xyz.zetabot.internals.enums.ZetaVersionChannel;
 
 public class ZetaSettings {
     private final String version = "0.0.1";
-    private final ZetaVersionChannel channel = ZetaVersionChannel.DEVELOP;
+    private final ZetaVersionChannel channel = ZetaVersionChannel.fromEnv();
 
     public String getVersion() {
         return version;
--- src/main/java/xyz/zetabot/internals/enums/ZetaVersionChannel.java
+++ src/main/java/xyz/zetabot/internals/enums/ZetaVersionChannel.java
@@ -1,12 +1,21 @@
 package xyz.zetabot.internals.enums;
 
 public enum ZetaVersionChannel {
-    STABLE("Stable"), BETA("Beta"), CANARY("Canary"), DEVELOP("Develop"), SPECIAL_EDITION("Special Edition");
-
+    STABLE("Stable"), BETA("Beta"), CANARY("Canary"), DEVELOP("Develop"), SPECIAL("Special Edition");
     private final String displayName;
 
     ZetaVersionChannel(String displayName) {
         this.displayName = displayName;
+    }
+
+    public static ZetaVersionChannel fromEnv() {
+        return switch (System.getenv("BOT_CHANNEL")) {
+            case "BETA" -> ZetaVersionChannel.BETA;
+            case "CANARY" -> ZetaVersionChannel.CANARY;
+            case "DEVELOP" -> ZetaVersionChannel.DEVELOP;
+            case "SPECIAL" -> ZetaVersionChannel.SPECIAL;
+            default -> ZetaVersionChannel.STABLE;
+        };
     }
 
     public String getDisplayName() {
